2b337e83c8213f31c31340689858dd63e806eeb1
SanvalEbert <115511942+SanvalEbert@users.noreply.github.com>
2025-07-15 21:17:44 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/GeovannaMaiaFreitas/', // <- Adicionamos isso
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
